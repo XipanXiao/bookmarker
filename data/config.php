52fa39ca11a8de21e757af5227d35e8463e555b6
Xipan Xiao <xxp@rodete-desktop-imager.corp.google.com>
2018-08-12 14:32:44 -0700

    add google cloud support

--- data/config.php
+++ data/config.php
@@ -5,12 +5,12 @@
     "password": "2345678901",
     "dbname": "bookmarker"
   },
-  "bookmarker-bicw.rhcloud.com": {
-    "servername": "${OPENSHIFT_MYSQL_DB_HOST}",
-    "username": "${OPENSHIFT_MYSQL_DB_USERNAME}",
-    "password": "${OPENSHIFT_MYSQL_DB_PASSWORD}",
-    "dbname": "bookmarker",
-    "session_path": "${OPENSHIFT_TMP_DIR}"
+  "sutra-reader.appspot.com": {
+    "servername": null,
+    "socket": "${MYSQL_SOCKET}",
+    "username": "${MYSQL_USERNAME}",
+    "password": "${MYSQL_PASSWORD}",
+    "dbname": "bookmarker"
   },
   "sutra-sutra.1d35.starter-us-east-1.openshiftapps.com": {
     "servername": "${MYSQL_SERVICE_HOST}",
--- php/connection.php
+++ php/connection.php
@@ -1,23 +1,24 @@
 <?php
    include_once 'config.php';
    include_once 'medoo.php';
- 
-  function get_medoo($charset = NULL) {
+
+   function get_medoo($charset = NULL) {
      global $config;
-    
+
     return new medoo([
         // required
         'database_type' => 'mysql',
         'database_name' => $config->dbname,
         'server' => $config->servername,
+        'socket' => $config->socket,
         'username' => $config->username,
         'password' => $config->password,
         'charset' => $charset == NULL ? 'utf8' : $charset,
-        
+
         // driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
         'option' => [
             PDO::ATTR_CASE => PDO::CASE_NATURAL
         ]
-    ]);    
+    ]);
   }
 ?>
